147e30dae74ddb4083d424263381884b4c1433dc
Kaua CaldeiraPC <kauacaldeira@hotmail.com>
2023-08-17 23:44:25 -0300

    testes concluido

---
+++ src/caixa-da-lanchonete.js
@@ -0,0 +1,124 @@
+import { Comida } from "./cardapio/comida.js";
+
+class CaixaDaLanchonete {
+
+  cafe = new Comida("cafe", "Café", 3.00)
+  chantily = new Comida("chantily", "Chantily (extra do Café)", 1.50)
+  suco = new Comida("suco", "Suco Natural", 6.20)
+  sanduiche = new Comida("sanduiche", "Sanduíche", 6.50)
+  queijo = new Comida("queijo", "Queijo (extra do Sanduíche)", 2.00)
+  salgado = new Comida("salgado", "Salgado", 7.25)
+  combo1 = new Comida("combo1", "1 Suco e 1 Sanduíche", 9.50)
+  combo2 = new Comida("combo2", "1 Café e 1 Sanduíche", 7.50)
+
+  cardapio = [this.cafe, this.chantily, this.suco, this.sanduiche, this.queijo, this.salgado, this.combo1, this.combo2]
+
+  calcularValorDaCompra(metodoDePagamento, items) {
+    try {
+      var valorTotalDoCarrinho = this.calcularValorTotalDoCarrinho(items);
+      return "R$ " + this.calcularPagamento(metodoDePagamento, valorTotalDoCarrinho).replace('.', ',');
+    } catch (error) {
+      return error.message;
+    }
+
+  }
+
+  calcularPagamento(metodoDePagamento, valorTotal) {
+
+    switch (metodoDePagamento) {
+      case "dinheiro":
+        return this.descontoEmDinheiro(valorTotal).toFixed(2);
+        break;
+
+      case "credito":
+        return this.acrescimoEmCredito(valorTotal).toFixed(2);
+        break;
+
+      case "debito":
+        return valorTotal.toFixed(2);
+        break;
+      default:
+        throw new Error("Forma de pagamento inválida!");
+    }
+
+  }
+
+  descontoEmDinheiro(valorTotal) {
+    return valorTotal -= valorTotal * 0.05;
+  }
+
+  acrescimoEmCredito(valorTotal) {
+    return valorTotal += valorTotal * 0.03;
+  }
+
+  calcularValorTotalDoCarrinho(items) {
+
+      var retorno = this.validarItemsExtrasDoCarrinho(items);
+      console.log(retorno + " <- AQUII ")
+
+      var valorTotal = 0;
+
+      if (items.length == 0) {
+        throw new Error("Não há itens no carrinho de compra!");
+      }
+
+      items.forEach(element => {
+        var arraySplit = element.split(',');
+
+        this.cardapio.map((e) => {
+
+          if (arraySplit[1] === undefined) {
+            throw new Error("Item inválido!")
+          }
+          else if (arraySplit[1] <= 0) {
+            throw new Error('Quantidade inválida!')
+          }
+          else if (arraySplit[0] === e.codigo) {
+            valorTotal += Number(arraySplit[1]) * e.valor;
+          }
+          else if (valorTotal == 0) {
+            throw new Error("Item inválido!")
+          } 
+
+        })
+
+      });
+      return Number(valorTotal);
+
+  }
+
+  validarItemsExtrasDoCarrinho(items) {
+    for (const element of items) {
+      const arraySplit = element.split(',');
+  
+      if (arraySplit[0] === "chantily") {
+        for (const element of items) {
+          const arraySplitChantily = element.split(',');
+          if (arraySplitChantily[0] === "cafe") {
+            return true;
+          }
+        }
+        throw new Error('Item extra não pode ser pedido sem o principal');
+      }
+  
+      if (arraySplit[0] === "queijo") {
+        for (const element of items) {
+          const arraySplitQueijo = element.split(',');
+          if (arraySplitQueijo[0] === "sanduiche") {
+            console.log("TESTEEEE");
+            return true;
+          }
+        }
+        throw new Error('Item extra não pode ser pedido sem o principal');
+      }
+    }
+  
+    return false; // Retorna falso se não houver nenhum cenário correspondente
+  }
+  
+}
+
+var caixa = new CaixaDaLanchonete();
+console.log(caixa.calcularValorDaCompra("credito", ['cafe,1', 'sanduiche,1', 'queijo,1']))
+
+export { CaixaDaLanchonete };
---
+++ src/cardapio/comida.js
@@ -0,0 +1,11 @@
+class Comida{
+
+    constructor(codigo, descricao, valor) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+}
+
+export { Comida };
